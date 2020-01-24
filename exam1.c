fb9a3854d32167897c13d7ab092359f0c671e8f5
20171cse0573 <60249366+20171cse0573@users.noreply.github.com>
2020-01-24 13:18:54 +0530

    Add files via upload

---
+++ exam1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{ 
+  int gender,age,m,f;
+  printf("enter the age& gender of the person\n");
+  scanf("%d\n",&age); 
+  if (gender==m,age>21)
+  { 
+    printf("eligible to the marriage\n");
+  }
+   else
+   {
+    printf("not eligible to the marriage\n");
+   }
+ }
